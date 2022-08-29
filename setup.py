7bbd3601b09e981b7268ce81c25508ab677b4a47
xingke.lwp <xingke.lwp@taobao.com>
2022-08-29 18:23:13 +0800

    fix package that misses queue_types

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 setup(
     name='eas-prediction',
-    version='0.14',
+    version='0.15',
     description='Python client sdk for eas inference service',
     url='http://gitlab.alibaba-inc.com/eas/eas-python-sdk',
-    packages=['eas_prediction'],
+    packages=['eas_prediction', 'eas_prediction/queue_types'],
     install_requires=['urllib3', 'protobuf==3.14', 'pyfarmhash', 'numpy>=1.9'])
