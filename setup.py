be5fe3faa86805fdc5ae68847b2f4ac95d3e7185
xingke.lwp <xingke.lwp@taobao.com>
2022-10-24 13:43:43 +0800

    add websocket-client dependency required by queue client

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 setup(
     name='eas-prediction',
-    version='0.16',
+    version='0.17',
     description='Python client sdk for eas inference service',
     url='http://gitlab.alibaba-inc.com/eas/eas-python-sdk',
     packages=['eas_prediction', 'eas_prediction/queue_types'],
-    install_requires=['urllib3', 'protobuf==3.14', 'numpy>=1.9'])
+    install_requires=['urllib3', 'protobuf==3.14', 'websocket-client', 'numpy>=1.9'])
