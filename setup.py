730f410f11699da9d2579f7f0d14edc1f9e7630b
realWeilai <543256949@qq.com>
2023-03-27 15:59:42 +0800

    add protobuf version limit (#10)
    Co-authored-by: lingcai.wl <lingcai.wl@alibaba-inc.com>

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 setup(
     name='eas-prediction',
-    version='0.18',
+    version='0.19',
     description='Python client sdk for eas inference service',
     url='http://gitlab.alibaba-inc.com/eas/eas-python-sdk',
     packages=['eas_prediction', 'eas_prediction/queue_types'],
-    install_requires=['urllib3', 'protobuf==3.14', 'websocket-client', 'numpy>=1.9'])
+    install_requires=['urllib3', 'protobuf>=3.6.0', 'numpy>=1.9', 'websocket-client'])
